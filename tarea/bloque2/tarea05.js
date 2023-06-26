b28262893258f33e2ef5754e3b3b0f982d7bcece
jordi <jordivallsplasmx@gmail.com>
2023-06-26 17:40:50 +0200

    tarea

--- tarea/bloque2/tarea05.js
+++ tarea/bloque2/tarea05.js
@@ -1,1 +1,13 @@
-console.log('Hola');
+let mes = 'Abril';
+
+switch (mes) {
+    case 'Enero' || 'Marzo' || 'Mayo' || 'Julio' || 'Agosoto' || 'Octubre' || 'Diciembre':
+        console.log(`El mes ${mes} tiene 31 días`);
+        break;
+    case 'Febrero':
+        console.log(`El mes ${mes} tiene 28/29 días`);
+        break;
+    case 'Abril' || 'Junio' || 'Setiembre' || 'Noviembre':
+        console.log(`El mes  ${mes} tiene 30 días`);
+        break;
+}
