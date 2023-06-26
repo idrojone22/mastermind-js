67b36178aa12541ce23cc3ad442f14c98f75fc4c
jordi <jordivallsplasmx@gmail.com>
2023-06-26 17:30:57 +0200

    tarea

--- tarea/bloque1/tarea04.js
+++
@@ -1,8 +0,0 @@
-let primero = 20;
-let segundo = 2;
-
-if (primero % segundo === 0) {
-    console.log(`${primero} es multiplo de ${segundo}`);
-}  else {
-    console.log(`${primero} no es múltiplo de ${segundo}`);
-}
